eae05d10dd37905ae0fe6dc284d9c3d86f968755
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-10-20 00:11:10 -0400

    sets up main component with material-ui component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Nav from "./Nav";
+import Main from "./Main";
 
 function App() {
   return (
     <div className="App">
-      <Nav name={"jaclyn"} />
+      <Main />
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,63 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import MapIcon from "@mui/icons-material/Map";
+
+const drawerWidth = 240;
+
+export default function Main() {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div"></Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <h1 className="app-name">Market Finder</h1>
+        <Divider />
+        <List>
+          {["Map", "<3", "Account", "Logout"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <MapIcon /> : <FavoriteIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
+      >
+        <Toolbar />
+      </Box>
+    </Box>
+  );
+}
