92aaed282f9a4de5b33a2eb0e9e518099475a262
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-10-21 00:01:07 -0400

    renders google map in Map component

--- src/Map.js
+++ src/Map.js
@@ -1,8 +1,25 @@
+import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
 function Map() {
+  const mapStyles = {
+    height: "100vh",
+    width: "100%",
+  };
+
+  const defaultCenter = {
+    lat: 41.3851,
+    lng: 2.1734,
+  };
+
   return (
-    <div className="Map">
-      <h1>Map Goes Here</h1>
-    </div>
+    <LoadScript googleMapsApiKey="AIzaSyBx3CmMKzArhN9tC7vKM2HPi1UcoXm0mjo">
+      <GoogleMap
+        mapContainerStyle={mapStyles}
+        zoom={13}
+        center={defaultCenter}
+      />
+    </LoadScript>
   );
 }
 
