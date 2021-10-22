e37810b02bef2e297f0a7a612771b757b00399f1
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-10-21 23:39:35 -0400

    adds useState hook

--- src/Map.js
+++ src/Map.js
@@ -1,27 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 function Map() {
+  //
+  const [currentPosition, setCurrentPosition] = useState({});
+
   const mapStyles = {
     height: "80vh",
     width: "100%",
   };
 
+  useEffect(() => {
+    !navigator.geolocation
+      ? alert("Your browser doesn't support Geolocation")
+      : navigator.geolocation.getCurrentPosition(success, err);
+  });
+
+  function success(position) {
+    const currentPosition = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    };
+    setCurrentPosition(currentPosition);
+  }
+
+  function err() {
+    alert("Failed to get your location");
+  }
+
   const defaultCenter = {
     lat: 41.3851,
     lng: 2.1734,
   };
-
-  useEffect(() => {
-    location();
-  });
-
-  function location() {
-    //check if Geolocation API is supported
-    !navigator.geolocation
-      ? alert("Your browser doesn't support Geolocation")
-      : alert("Getting your location!");
-  }
 
   return (
     <LoadScript googleMapsApiKey="AIzaSyBx3CmMKzArhN9tC7vKM2HPi1UcoXm0mjo">
