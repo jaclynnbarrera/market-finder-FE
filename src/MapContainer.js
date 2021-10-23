8616b2024259117216c23af5a13c88d20f683da9
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-10-22 22:04:59 -0400

    sets up MapContainer component to house both Map and Results components

---
+++ src/MapContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Map from "./Map";
+import Usda from "./Usda";
+
+function MapContainer() {
+  return (
+    <div>
+      <Map />
+      <Usda />
+    </div>
+  );
+}
+
+export default MapContainer;
