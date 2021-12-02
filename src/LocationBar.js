ab5e3e29a367c1904be50f952905656421a77184
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-12-02 18:44:30 -0500

    imports material ui search icon into LoctionBar component

--- src/LocationBar.js
+++ src/LocationBar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AddLocationIcon from "@mui/icons-material/AddLocation";
+import SearchIcon from "@mui/icons-material/Search";
 
 function LocationBar() {
   return (
     <div>
       <AddLocationIcon />
+      <SearchIcon />
     </div>
   );
 }
