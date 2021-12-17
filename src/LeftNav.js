87822eec3fb896fdb41ffc465c253cdba9a61d82
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-12-16 23:40:20 -0500

    styling

--- src/LeftNav.js
+++ src/LeftNav.js
@@ -4,10 +4,10 @@ function LeftNav() {
   return (
     <div className="leftnav">
       <h1 className="app-name">MARKET FINDER</h1>
-      <p>menu item</p>
-      <p>menu item</p>
-      <p>menu item</p>
-      <p>menu item</p>
+      <p>Map</p>
+      <p>Favorites</p>
+      <p>Account</p>
+      <p>Logout</p>
     </div>
   );
 }
