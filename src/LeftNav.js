53b4f95b623b84b487ab79bb17803c1434654b3e
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-12-17 00:11:44 -0500

    adds material ui icons

--- src/LeftNav.js
+++ src/LeftNav.js
@@ -1,21 +1,34 @@
 import React from "react";
-import MapIcon from '@mui/icons-material/Map';
+import MapIcon from "@mui/icons-material/Map";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LogoutIcon from "@mui/icons-material/Logout";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
 
 function LeftNav() {
   return (
     <div className="leftnav">
       <h1 className="app-name">MARKET FINDER</h1>
-      <div className="menu-item" >
-      <MapIcon/>
-      <p>Map</p>
+      <div className="menu-item">
+        <MapIcon />
+        <p>Map</p>
       </div>
 
-      <p>Favorites</p>
-      <p>Account</p>
-      <p>Logout</p>
+      <div className="menu-item">
+        <FavoriteIcon />
+        <p>Favorites</p>
+      </div>
+
+      <div className="menu-item">
+        <ManageAccountsIcon />
+        <p>Account</p>
+      </div>
+
+      <div className="menu-item">
+        <LogoutIcon />
+        <p>Logout</p>
+      </div>
     </div>
   );
 }
 
-export default LeftNav
-
+export default LeftNav;
