d8210cc514166be570186d6758f20c816a6cb652
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-12-17 16:10:56 -0500

    begin styling on top nav buttons

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,8 +1,9 @@
 import React from "react";
+
 function Nav() {
   return (
     <div className="topbar">
-      <button>button 1</button>
+      <button className="button">Use Current Location</button>
       <button>button 1</button>
       <button>button 1</button>
       <button>button 1</button>
