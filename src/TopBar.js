87f12bf375faa4ae56a7cf170530c7afb5a25283
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-12-17 00:42:57 -0500

    flexbox fun

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,8 +1,14 @@
 import React from "react";
 function Nav() {
   return (
-    <div className="topbar">top bar</div>
+    <div className="topbar">
+      <button>button 1</button>
+      <button>button 1</button>
+      <button>button 1</button>
+      <button>button 1</button>
+      <button>button 1</button>
+    </div>
   );
 }
 
-export default Nav
+export default Nav;
